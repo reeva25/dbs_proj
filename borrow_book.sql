bce7e625988d2723f75008de295e0668f362a386
reeva25 <117997905+reeva25@users.noreply.github.com>
2024-04-16 15:18:35 +0530

    Update borrow_book.sql

--- borrow_book.sql
+++ borrow_book.sql
@@ -3,11 +3,12 @@ bookname BOOK.title%type;
 usid users.userid%type;
 bid book.bookid%type;
 borr borrow.borrowid%type;
+flag int;
 
 cursor c(bid book.bookid%type) is select * from inventory where bookid=bid;
 
 begin
-
+flag := 0;
 bookname := 'BOOKNAME';
 usid := 'USERID';
 
@@ -15,6 +16,7 @@ select bookid into bid from book where title=bookname;
 
 for item in c(bid) loop
 if item.available='YES' then 
+	flag := 1;
 	update inventory set available='NO' where bookid=item.bookid and InventoryNo=item.InventoryNo;
 
 update book set AvailableQty= AvailableQty-1 where bookid=item.bookid;
@@ -25,5 +27,8 @@ update book set AvailableQty= AvailableQty-1 where bookid=item.bookid;
 	EXIT;
 END IF;
 end loop;
+if flag=0 then
+    dbms_output.put_line('book unavailable');
+end if;
 END;
 /
