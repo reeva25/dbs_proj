d575014a7753a67289c46ca8971202128b3885c4
reeva25 <117997905+reeva25@users.noreply.github.com>
2024-04-16 15:05:34 +0530

    Update add_book.sql

--- add_book.sql
+++ add_book.sql
@@ -1,60 +1,8 @@
--- ADD BOOK 
-Create or replace procedure add_book is
-	T_BID BOOK.BOOKID%TYPE;
-   	T_AUTHID BOOK.AUTHID%TYPE;
-	ANAME AUTHOR.NAME%TYPE;
-	GNAME GENRE.GenreName%TYPE;
-	T_GENREID BOOK.GENREID%TYPE;
-	T_TITLE BOOK.TITLE%TYPE;
-	T_TOTQTY BOOK.TOTALQTY%TYPE;
-    A_QTY BOOK.AvailableQty%TYPE;
-	flag number:=0;
-    quant number ;
-	inventory_id number;
-	cursor c_book is select title from book for update;
-	begin
-	t_bid:='&book_id' ;
-	t_title:='&book_title';
-	aname:='&author_name';
-	gname:='&genre_name';
 
-    for item in c_book loop
-	if t_title=item.title then
-		flag:=1;
-		UPDATE Book  SET AvailableQty = AvailableQty + 1
-		WHERE title=item.title;
-		UPDATE Book  SET TotalQty = TotalQty + 1
-		WHERE title=item.title;
-        select TotalQty into quant from book where title = item.title ;
-		Insert into inventory values(t_bid, quant,'YES');
-	end if;
-	end loop;
-
-	if flag=0 then
-		select authid into T_AUTHID from author where name=aname;
-		select genreid into t_genreid from genre where GenreName=gname;
-		if t_authid is null then
-			add_author(aname);
-		end if;
-		if t_genreid is null then
-			add_genre(gname);
-		end if;
-		BEGIN
-			insert into book values(t_bid,t_authid,t_genreid,t_title,1,1);
-			Insert into inventory values(t_bid, 1,'YES');
-		EXCEPTION
-                WHEN DUP_VAL_ON_INDEX THEN
-                    DBMS_OUTPUT.PUT_LINE('Error: Book with ID ' || T_BID || ' already exists.');
-                WHEN OTHERS THEN
-                    DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
-            END;
-end if;
-End;
-/
 Create or replace procedure add_author(aname author.name%type)  is
 	t_auth_id author.authid%type;
 	begin
-	t_auth_id := '&authorid';
+        select count(authid)+1 into t_auth_id from author;
 	insert into author values (t_auth_id,aname);
 	exception
 	  WHEN DUP_VAL_ON_INDEX THEN
@@ -67,7 +15,7 @@ Create or replace procedure add_author(aname author.name%type)  is
 Create or replace procedure add_genre(gname genre.GenreName%type) is
 t_gid genre.GenreID%type;
 	begin
-	t_gid := '&genreid';
+	select count(genreid)+1 into t_gid from genre;
 	insert into genre values (t_gid,gname);
 	exception
 	  WHEN DUP_VAL_ON_INDEX THEN
@@ -75,5 +23,76 @@ t_gid genre.GenreID%type;
                 WHEN OTHERS THEN
                     DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
         END;
+/
+Create or replace procedure add_book is
+	T_BID BOOK.BOOKID%TYPE;
+   	T_AUTHID BOOK.AUTHID%TYPE;
+	ANAME AUTHOR.NAME%TYPE;
+	GNAME GENRE.GenreName%TYPE;
+	T_GENREID BOOK.GENREID%TYPE;
+	T_TITLE BOOK.TITLE%TYPE;
+	T_TOTQTY BOOK.TOTALQTY%TYPE;
+    A_QTY BOOK.AvailableQty%TYPE;
+	flag int;
+    quant BOOK.TOTALQTY%TYPE;
+	inventory_id BOOK.TOTALQTY%TYPE;
+	cursor c_book is select title from book for update;
+	begin
+    flag := 0;
+	t_title:='&title';
+	aname:='&authr';
+	gname:='&genre';
+
+	select count(bookid)+1 into t_bid from book;
+	dbms_output.put_line(t_bid);
+
+    for item in c_book loop
+	if t_title=item.title then
+		flag:=1;
+		UPDATE Book  SET AvailableQty = AvailableQty + 1
+		WHERE title=item.title;
+		UPDATE Book  SET TotalQty = TotalQty + 1
+		WHERE title=item.title;
+      	select TotalQty into quant from book where title = item.title ;
+		select bookid into t_bid from book where title=item.title;
+		Insert into inventory values(t_bid, quant,'YES');
+	end if;
+	end loop;
+
+	if flag=0 then
+        begin
+			select authid into T_AUTHID from author where name=aname;
+		EXCEPTION
+    		WHEN NO_DATA_FOUND THEN
+				add_author(aname);
+				select authid into T_AUTHID from author where name=aname;
+			
+		end;
+		begin
+			select genreid into t_genreid from genre where GenreName=gname;
+		EXCEPTION
+    		WHEN NO_DATA_FOUND THEN
+				add_genre(gname);
+				select genreid into t_genreid from genre where GenreName=gname;
+				
+		end;
+		
+		BEGIN
+			insert into book values(t_bid,t_authid,t_genreid,t_title,1,1);
+			Insert into inventory values(t_bid, 1,'YES');
+		EXCEPTION
+                WHEN DUP_VAL_ON_INDEX THEN
+                    DBMS_OUTPUT.PUT_LINE('Error: Book with ID ' || T_BID || ' already exists.');
+                WHEN OTHERS THEN
+                    DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+            END;
+end if;
+End;
+/
+
+set serveroutput on;
+begin
+add_book;
+end;
 /
 
