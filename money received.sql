75e5e82849cf1cc33a6298dbeda5d836ca3a4672
Dev Vasudevan <119096133+Dev-Vasudevan@users.noreply.github.com>
2024-04-17 02:40:21 +0530

    Update money received.sql

--- money received.sql
+++ money received.sql
@@ -1,34 +1,27 @@
 
-Create or replace procedure moneyreceived(purpose varchar2)
-As
+Create or replace procedure moneyreceived(purpose varchar2, id users.UserID%type, p_method Dues.Method%TYPE) As
 
-
-u_id users.userid%type;
 did dues.dueid%type;
-p_method Dues.Method%TYPE;
+
 
 bid book.bookid%type;
-borrid borrow.borrowid%type;
 invid borrow.inventoryid%type;
 
 Begin 
 
-u_id := '&USERID';
-p_method := '&paymentmethod';
 
 If purpose='SUBSCRIPTION' then
-Select dueid into did from UsersSubscriptionDues where usersid=u_id;
+Select dueid into did from UsersSubscriptionDues where usersid=id;
 Update dues set DateOfReceipt=sysdate where dueid=did;
 Update dues set Paid ='YES' where dueid=did;
 Update dues set method=p_method where dueid=did;
 
 ELSE
-borrid := '&BORROWID';
 
-Select duesid into did from borrow_dues where borrowid=borrid;
-select bookid into bid from borrow where borrowid=borrid;
+Select duesid into did from borrow_dues where borrowid=id;
+select bookid into bid from borrow where borrowid=id;
 
-select inventoryid into invid from borrow where borrowid=borrid;
+select inventoryid into invid from borrow where borrowid=id;
 
 
 
