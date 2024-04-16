4b71f71a1f55ea5d058559d3435f6caa8d126961
reeva25 <117997905+reeva25@users.noreply.github.com>
2024-04-16 20:32:18 +0530

    Update borrow_book.sql

--- borrow_book.sql
+++ borrow_book.sql
@@ -3,6 +3,8 @@ bookname BOOK.title%type;
 usid users.userid%type;
 bid book.bookid%type;
 borr borrow.borrowid%type;
+Stype subscription.subtype%type;
+
 flag int;
 
 cursor c(bid book.bookid%type) is select * from inventory where bookid=bid;
@@ -16,19 +18,34 @@ select bookid into bid from book where title=bookname;
 
 for item in c(bid) loop
 if item.available='YES' then 
-	flag := 1;
+    flag :=1;
 	update inventory set available='NO' where bookid=item.bookid and InventoryNo=item.InventoryNo;
 
 update book set AvailableQty= AvailableQty-1 where bookid=item.bookid;
 
 	select count(borrowid)+1 into borr from borrow;	
-
-	insert into borrow values( borr, SYSDATE, SYSDATE + INTERVAL '15' DAY, item.InventoryNo,item.bookid,usid,'borrowed');
+	select subtype into stype from UsersSubscriptionDues where usersid=usid;
+	if stype='GOLD' then
+		insert into borrow values( borr, SYSDATE, SYSDATE + INTERVAL '15' DAY, item.InventoryNo,item.bookid,usid,'borrowed');
+	elsif stype='SILVER' then
+		insert into borrow values( borr, SYSDATE, SYSDATE + INTERVAL '10' DAY, item.InventoryNo,item.bookid,usid,'borrowed');
+	elsif stype='PLATINUM' then
+		insert into borrow values( borr, SYSDATE, SYSDATE + INTERVAL '25' DAY, item.InventoryNo,item.bookid,usid,'borrowed');
+	end if;
 	EXIT;
 END IF;
 end loop;
+
 if flag=0 then
     dbms_output.put_line('book unavailable');
 end if;
 END;
 /
+
+set serveroutput on;
+begin
+    borrow_book;
+end;
+/
+select * from inventory;
+select * from borrow;
